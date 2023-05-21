4146d8201ca224bc0e7f5132a9a31492d7fe26a4
difaananda40 <difacool40@gmail.com>
2023-05-21 18:34:38 +0800

    change: home template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.layout')
+
+@section('content')
+    <!-- Jumbotron -->
+	<div class="jumbotron text-center">
+		<h1>WELCOME TO AHMAD LIBRARY JANCO</h1> 
+		<p>Search Your Books U Want From Us</p> 
+	</div>
+	<!-- End Jumbotron -->
+
+	<!-- Konten -->
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<h3>HOME COMING PART I</h3>
+				<p>The Beginning of our Journey.</p>
+			</div>
+			<div class="col-sm-4">
+				<h3>HOME COMING PART II</h3>
+				<p>Second Journey, When The Troubles Comes.</p>
+			</div>
+			<div class="col-sm-4">
+				<h3>HOME COMING PART III</h3>
+				<p>Last Part, This Is How The Journey Ends.</p>
+			</div>
+		</div>
+	</div>
+	<!-- End Konten -->
+@endsection
