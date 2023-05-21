fd0ab18096aa89a2bdf0f8d975a4a7834239bdb7
difaananda40 <difacool40@gmail.com>
2023-05-22 04:36:53 +0800

    change: update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,10 @@
 <x-app-layout>
 	<div class="py-12">
-			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-					<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-							<div class="p-6 text-gray-900">
-									{{ __("WELCOME!") }}
-							</div>
-					</div>
+			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col justify-center items-center space-y-10">
+				<h1 class="mt-24 text-center text-white font-serif text-7xl font-bold leading-snug">
+					Start your day with <br>our coffee.
+				</h1>
+				<a href="#menu" class="text-xl bg-yellow-100 text-stone-800 px-4 py-2 rounded">See menus</a>
 			</div>
 	</div>
 </x-app-layout>
