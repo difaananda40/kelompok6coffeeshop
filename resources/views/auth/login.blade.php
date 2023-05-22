31f84c2f496bcb524eae08472610fec6f9282054
difaananda40 <difacool40@gmail.com>
2023-05-22 17:16:48 +0800

    add: register button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,6 +32,12 @@
             </label>
         </div>
 
+        <div class="flex items-center justify-center mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Not registered? Register here.') }}
+            </a>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
