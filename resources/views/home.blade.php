1334fa4538ba6f9dd051cd6f9238e6ee1d4cf394
difaananda40 <difacool40@gmail.com>
2023-05-22 17:16:56 +0800

    change: text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,10 @@
 	<div class="py-12">
 			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col justify-center items-center space-y-10">
 				<h1 class="mt-24 text-center text-white font-serif text-7xl font-bold leading-snug">
-					Start your day with <br>our coffee.
+					"Enjoy your <span class="text-yellow-100/95">day</span> with coffee<br>from
+					<span class="text-yellow-100/95">Kopi Sendja."</span>
 				</h1>
-				<a href="#menu" class="text-xl bg-yellow-100 text-stone-800 px-4 py-2 rounded">See menus</a>
+				<a href="#menu" class="text-xl bg-yellow-100/95 text-stone-800 px-4 py-2 rounded hover:brightness-90 transition-all">See menus</a>
 			</div>
 	</div>
 </x-app-layout>
